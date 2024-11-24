465839d957eab2f0e6dd134cee32e55ad2732fba
Ali Qayyoomi <aliqbhastam@gmail.com>
2024-11-24 19:27:02 +0330

    add basic view

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { NavigationComponent } from './layout/navigation/navigation.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,MatSlideToggleModule],
+  imports: [RouterOutlet,MatSlideToggleModule,NavigationComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
